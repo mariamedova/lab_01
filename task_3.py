1539cebf24adb8a4072398ed50ebc59d4608122d
mariamedova <151378727+mariamedova@users.noreply.github.com>
2026-09-20 15:53:02 +0300

    Update task_3.py
    добавлены эксперименты B, C, D

--- task_3.py
+++ task_3.py
@@ -1,4 +1,5 @@
-x = 17
+#--------A--------
+"""x = 17
 y = 5
 
 print('x + y =', x + y)
@@ -7,4 +8,32 @@ print('x * y =', x * y)
 print('x / y =', x / y)
 print('x // y =', x // y)
 print('x % y =', x % y)
-print('x ** y =', x ** y)
+print('x ** y =', x ** y)"""
+
+#--------B--------
+
+'''print('2 ** 1000 =', 2 ** 1000)'''
+
+#--------C--------
+
+'''import math
+
+result = 0.1 + 0.2
+
+print('result:', result)
+print('result == 0.3:', result == 0.3)
+print('result - 0.3 =', result - 0.3 )
+print('math.isclose(result, 0.3):', math.isclose(result, 0.3))'''
+
+#--------D--------
+
+'''print(int("42"), type(int("42")))
+print(float("3.14"), type(float("3.14")))
+print(str(2026), type(str(2026)))
+print(bool(0), type(bool(0)))
+print(bool(-1), type(bool(-1)))
+print(bool(""), type(bool("")))
+print(bool("False"), type(bool("False")))'''
+print(complex(2, -3), type(complex(2, -3)))
+print('действительная часть:', {complex(2, -3).real})
+print('мнимая часть:', {complex(2, -3).imag})
