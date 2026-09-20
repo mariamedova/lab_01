ff2ca5ea18269261916f33fb2f271bb76b61074b
mariamedova <151378727+mariamedova@users.noreply.github.com>
2026-09-20 14:24:46 +0300

    Update task_2.py
    Добавлены части 2 и 3

--- task_2.py
+++ task_2.py
@@ -1,4 +1,4 @@
-a = 1000
+'''a = 1000
 b = a
 c = int("1000")
 
@@ -8,3 +8,16 @@ print("a == b:", a == b) #True
 print("a is b:", a is b) #True
 print("a == c:", a == c) #True
 print("a is c:", a is c) #False
+
+#---------------------
+
+c = None
+print('c is None:', c is None)
+'''
+#---------------------
+
+first = "python"
+second = "py" + "thon"
+
+print(first == second)
+print(first is second)
