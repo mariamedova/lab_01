89c03de03145007a7ba62aaecd195e8d5b2185f4
mariamedova <151378727+mariamedova@users.noreply.github.com>
2026-09-20 14:30:09 +0300

    Create task_3.py
    Эксперимент A

---
+++ task_3.py
@@ -0,0 +1,10 @@
+x = 17
+y = 5
+
+print('x + y =', x + y)
+print('x - y =', x - y)
+print('x * y =', x * y)
+print('x / y =', x / y)
+print('x // y =', x // y)
+print('x % y =', x % y)
+print('x ** y =', x ** y)
